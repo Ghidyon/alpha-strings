234339b861b4ebe6722e3b6f340343196d9e3854
Ghidyon <gideonakunana@gmail.com>
2020-09-08 22:34:14 +0100

    third commit

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const sentence = "gHIDyon WriTIng And ScriPtiNg In THe eYe blOCK Of tHE CiVIc ceNtER";
 let reversedCase = "";
-let wordUpperCase = [];
-let ingSuffix = [];
+const wordUpperCase = [];
+const ingSuffix = [];
+const palindromeList = [];
 
+// Case Conversion
 const convertCase = (string) => {
     for (let i = 0; i < string.length; i++) {
         let character = string.charAt(i);
@@ -20,10 +22,12 @@ const convertCase = (string) => {
 
 convertCase(sentence);
 
+
 const caseArray = reversedCase.split(" ");
 console.log(caseArray);
+const arrLength = caseArray.length;
 
-for (let i = 0; i < caseArray.length; i++) {
+for (let i = 0; i < arrLength; i++) {
     let firstLetter = caseArray[i].charAt(0);
 
     if (firstLetter === firstLetter.toUpperCase()) {
@@ -35,10 +39,11 @@ console.log(sentence);
 console.log(reversedCase);
 console.log(wordUpperCase);
 
-for (let i = 0; i < caseArray.length; i++) {
+for (let i = 0; i < arrLength; i++) {
     let matchIng = caseArray[i].match(/ing/gi);
 
     if (matchIng == "iNG") {
+        console.log(caseArray);
         ingSuffix.push(caseArray[1]);
     }
 
@@ -47,4 +52,26 @@ for (let i = 0; i < caseArray.length; i++) {
     }
 }
 
-console.log(ingSuffix)
+console.log(ingSuffix);
+
+// Palindrome
+const palindromeChecker = (array) => {
+    for (let i = 0; i < arrLength; i++) {
+        let caseArrayElement = array[i].toLowerCase();
+        let reversedElement = caseArrayElement.split("").reverse("").join("");
+
+        if (caseArrayElement === reversedElement) {
+            palindromeList.push(caseArrayElement);
+        }
+    }
+    console.log(palindromeList);
+}
+
+/* const sentenceObjectItem = () => {
+
+    const sentenceArrayObject = {
+        uppercased: ,
+        palindrome: palindromeChecker(caseArray);
+
+    }
+} */
